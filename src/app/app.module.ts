e8e418ade04249db10ee5353680c00f143a1c9dd
Ismael Melo <ismaelsmelo84@gmail.com>
2019-02-16 13:08:33 -0200

    Criando interceptação no app.module para o uso de API interna (mocada) - remover isso e o arquivo in-memory-database ao usar a api externa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; // Remover ao usar API externa
+import { InMemoryDatabase } from './in-memory-database'; // Remover ao usar API externa
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) // Remover ao usar API externa
   ],
   providers: [],
   bootstrap: [AppComponent]
